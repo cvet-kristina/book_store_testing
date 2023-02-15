9e03c3e650eb7c7a1362dcef02a740fec3524342
Kristina Morgunova <sofirenar@gmail.com>
2023-02-15 14:00:48 +0300

    Registration_login: регистрация аккаунта

---
+++ login_registration.py
@@ -0,0 +1,17 @@
+# # Регистрация аккаунта
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.get("http://practice.automationtesting.in/")
+#
+# my_account = driver.find_element_by_css_selector("#menu-item-50 > a")
+# my_account.click()
+# reg_email = driver.find_element_by_id("reg_email")
+# reg_email.send_keys("mail@mail.com")
+# reg_password = driver.find_element_by_id("reg_password")
+# reg_password.send_keys("ExtraPass!")
+# register_btn = driver.find_element_by_css_selector("[value='Register']")
+# register_btn.click()
+#
+# driver.quit()
