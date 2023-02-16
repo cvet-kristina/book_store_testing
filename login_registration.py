7f7a706acbd76abea2596375330c03f033d24070
Kristina Morgunova <sofirenar@gmail.com>
2023-02-16 20:53:58 +0300

    Добавила implicitly_wait во все тесты; раскомментировала тесты

--- login_registration.py
+++ login_registration.py
@@ -1,20 +1,21 @@
-# # Регистрация аккаунта
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome()
-# driver.maximize_window()
-# driver.get("http://practice.automationtesting.in/")
-#
-# my_account = driver.find_element_by_css_selector("#menu-item-50 > a")
-# my_account.click()
-# reg_email = driver.find_element_by_id("reg_email")
-# reg_email.send_keys("super@mail.com")
-# reg_password = driver.find_element_by_id("reg_password")
-# reg_password.send_keys("mypassissuper")
-# register_btn = driver.find_element_by_css_selector("[value='Register']")
-# register_btn.click()
-#
-# driver.quit()
+# Регистрация аккаунта
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://practice.automationtesting.in/")
+driver.implicitly_wait(5)
+
+my_account = driver.find_element_by_css_selector("#menu-item-50 > a")
+my_account.click()
+reg_email = driver.find_element_by_id("reg_email")
+reg_email.send_keys("super@mail.com")
+reg_password = driver.find_element_by_id("reg_password")
+reg_password.send_keys("mypassissuper")
+register_btn = driver.find_element_by_css_selector("[value='Register']")
+register_btn.click()
+
+driver.quit()
 
 # Логин в систему
 from selenium import webdriver
@@ -25,6 +26,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("http://practice.automationtesting.in/")
+driver.implicitly_wait(5)
 
 my_account = driver.find_element_by_css_selector("#menu-item-50 > a")
 my_account.click()
